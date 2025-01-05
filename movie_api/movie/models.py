1a8dcd3c47595c5ff116bbdd313604b6351fd5a7
Orit Samuel <oritsamuelseid@gmail.com>
2025-01-05 14:52:57 +0300

    api endpoints

--- movie_api/movie/models.py
+++ movie_api/movie/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=255)
+    order_date = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product, through='OrderProduct')
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
